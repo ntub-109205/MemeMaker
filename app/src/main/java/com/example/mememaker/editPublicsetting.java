3b78f953726bd01236760d3673cb006be7c6f0c2
Angela822 <angelayu822@gmail.com>
2020-04-12 22:31:41 +0800

    editPublicsetting介面已修正

--- app/src/main/java/com/example/mememaker/editPublicsetting.java
+++ app/src/main/java/com/example/mememaker/editPublicsetting.java
@@ -1,14 +1,73 @@
 package com.example.mememaker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
 
 public class editPublicsetting extends AppCompatActivity {
+
+    Switch switchTemp;
+    Switch switchMeme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_publicsetting);
+
+        //switch
+        switchTemp = findViewById(R.id.tempSwitch);
+        switchMeme = findViewById(R.id.memeSwitch);
+
+        //將switch的狀態儲存到shared preferences
+        SharedPreferences sharedPreferences = getSharedPreferences("save",MODE_PRIVATE);
+        //---switchTemp---
+        switchTemp.setChecked(sharedPreferences.getBoolean("value",true));
+
+        switchTemp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (switchTemp.isChecked()) {
+                    //當switch被觸發
+                    SharedPreferences.Editor editor = getSharedPreferences("save",MODE_PRIVATE).edit();
+                    editor.putBoolean("value",true);
+                    editor.apply();
+                    switchTemp.setChecked(true);
+                }else{
+                    //當switch沒被觸發
+                    SharedPreferences.Editor editor = getSharedPreferences("save",MODE_PRIVATE).edit();
+                    editor.putBoolean("value",false);
+                    editor.apply();
+                    switchTemp.setChecked(false);
+                }
+            }
+        });
+
+        //---switchMeme---
+        switchMeme.setChecked(sharedPreferences.getBoolean("value",true));
+
+        switchMeme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (switchMeme.isChecked()) {
+                    //當switch被觸發
+                    SharedPreferences.Editor editor = getSharedPreferences("save",MODE_PRIVATE).edit();
+                    editor.putBoolean("value",true);
+                    editor.apply();
+                    switchMeme.setChecked(true);
+                }else{
+                    //當switch沒被觸發
+                    SharedPreferences.Editor editor = getSharedPreferences("save",MODE_PRIVATE).edit();
+                    editor.putBoolean("value",false);
+                    editor.apply();
+                    switchMeme.setChecked(false);
+                }
+            }
+        });
+
+
     }
 }
